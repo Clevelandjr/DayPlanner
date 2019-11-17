f619fece29d04d7561115bfd37a2669b4d958e23
Cleveland Pitt Jr <clevelandpittjr@Clevelands-MacBook-Pro.local>
2019-11-17 03:46:50 -0500

    worked on local storage

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ window.onload = function () {
     console.log(input);
     localStorage.setItem(time, input);
  })
- function getSavedText() {
-    $("#9AM").val(localStorage.getItem("9AM"));
- }
- getSavedText();
+ ///storage///
+ 
+
+
